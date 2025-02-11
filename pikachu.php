7f475a134a7996def8a97ee19318916d1b8d1726
maludeventura <marialuizaalmeidaventura@gmail.com>
2025-02-11 14:50:53 -0300

    AULA01

---
+++ pikachu.php
@@ -0,0 +1,36 @@
+<?php include './header.php'; ?>
+<div class="container">
+    <div class="col-12 d-flex justify-content-center">
+        <div class ="col-6 p-3">
+        <div id="carouselExample" class="carousel slide">
+   <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://i.pinimg.com/236x/87/67/4b/87674b1aed73fc448bd021dbd96fb2a9.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStMnjLfDLh9kALpOOnYZ2G8mCFZqKRzLPYdM2tTvyWsMjyEdHQrjARMOVaOgjdx188FZI&usqp=CAU" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8hZSnB7FjsfXJ3TYSWscQgVI1FwLee6GWNtM8qi5DITOWJQzrmhi43R18ML8kKQ3bosE&usqp=CAU" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  </div>
+
+</div>
+</div>
+</div>
+
+
+
+    
+
+    <?php include './footer.php'; ?>
+?>
